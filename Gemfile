9bed330beb86dc81a7f06b3e15fd41c09f110329
Thorsten Claus <thorstenclaus@web.de>
2021-12-27 20:51:08 +0100

    Layout Posts

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem 'bootstrap', '~> 5.1.3'
 gem 'haml'
 
 gem 'pagy', '~> 5.6', '>= 5.6.6'
+gem 'local_time'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,30 @@
 class Profile < ApplicationRecord
   belongs_to :person
+
+  def image_url(size: :thumb_large, fallback_to_default: true)
+    result = if size == :thumb_medium && self[:image_url_medium]
+               self[:image_url_medium]
+             elsif size == :thumb_small && self[:image_url_small]
+               self[:image_url_small]
+             else
+               self[:image_url]
+             end
+    if result
+      result
+    else
+      ActionController::Base.helpers.image_path("user/default.png")
+    end
+  end
+
+  def image_url=(url)
+    super(build_image_url(url))
+  end
+
+  def image_url_small=(url)
+    super(build_image_url(url))
+  end
+
+  def image_url_medium=(url)
+    super(build_image_url(url))
+  end
 end
