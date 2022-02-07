99f990432c7ac6ec443d86abbe5d636aed73dd6e
Thorsten Claus <thorstenclaus@web.de>
2022-02-07 21:37:33 +0100

    Start by server script (#24)
    * Startup by server scipts
    Using Puma instead of unicorn
    
    Eye might be not used, but puma server now will be started as a system service
    
    * Assets check for an application file

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bin/bundle exec puma -C config/puma.rb -p $PORT
+sidekiq: bin/bundle exec sidekiq
---
+++ config/eye.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative "load_config"
+rails_env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
+
+Eye.config do
+  logger Logger.new(STDOUT)
+end
+
+Eye.application("diaspora") do
+  working_dir Rails.root.to_s
+  env "RAILS_ENV" => rails_env
+  stdout "log/eye_processes_stdout.log" unless rails_env == "development"
+  stderr "log/eye_processes_stderr.log"
+
+  process :web do
+    puma_command = "bin/bundle exec puma -C config/puma.rb"
+
+    if rails_env == "production"
+      start_command "#{puma_command} &"
+      daemonize false
+      restart_command "kill -USR1 {PID}"
+      restart_grace 10.seconds
+    else
+      start_command puma_command
+      daemonize true
+    end
+
+    pid_file AppConfig.server.pid.get
+    stop_signals [:TERM, 10.seconds]
+
+    env "PORT" => ENV["PORT"]
+
+    monitor_children do
+      stop_command "kill -QUIT {PID}"
+    end
+  end
+
+  group :sidekiq do
+    with_condition(!AppConfig.environment.single_process_mode?) do
+      AppConfig.server.sidekiq_workers.to_i.times do |i|
+        i += 1
+
+        process "sidekiq#{i}" do
+          start_command "bin/bundle exec sidekiq"
+          daemonize true
+          pid_file "tmp/pids/sidekiq#{i}.pid"
+          stop_signals [:USR1, 0, :TERM, 10.seconds, :KILL]
+        end
+      end
+    end
+  end
+end
